0354c692fba30e4b27a20b35943ea9c80699d0c0
Joel Ocay <jmocay72@gmail.com>
2020-05-30 13:14:03 -0400

    Added rotating armature

--- Graphics/SquareWave/fourier.js
+++ Graphics/SquareWave/fourier.js
@@ -2,6 +2,7 @@ let canvas
 let ctx
 let xTranslate
 let yTranslate
+let armature
 let fourier
 
 class FourierSeries {
@@ -10,7 +11,37 @@ class FourierSeries {
         this.lambda = initArgs.lambda
         this.freq = initArgs.freq
         this.harmonics = initArgs.n
-        this.reset()
+        this.t = 0
+    }
+
+    draw() {
+        this.t += 1
+        if (this.t == 360) {
+            this.t = 0
+        }
+
+        for (let x = 0; x < xTranslate; x++) {
+            let y = 0
+            for (let n = 1; n <= this.harmonics; n++) {
+                y += -Math.sin(
+                    (2 * n - 1) * 2 * Math.PI * (this.freq * this.t / 180 + x / this.lambda)
+                ) / (2 * n - 1)
+            }
+            y = this.amp * y
+            ctx.fillStyle = rgbToHex(255, 255, 255)
+            ctx.beginPath()
+            ctx.arc(x, y, 2, 0, 2 * Math.PI)
+            ctx.fill()
+        }
+    }
+}
+
+class Armature {
+    constructor(initArgs) {
+        this.amp = initArgs.amp
+        this.freq = initArgs.freq
+        this.harmonics = initArgs.n
+        this.t = 0
     }
 
     draw() {
@@ -19,18 +50,38 @@ class FourierSeries {
             this.reset()
         }
 
-        for (let x = 0; x < xTranslate; x++) {
-            let y = 0
-            for (let n = 1; n <= this.harmonics; n++) {
-                y += Math.sin(
-                    (2 * n - 1) * 2 * Math.PI * (this.freq * this.t / 180 + x / this.lambda)
-                ) / (2 * n - 1)
-            }
-            y = -this.amp * y
+        let xOffset = xTranslate / 2
+        let x0 = 0
+        let y0 = 0
+        let tipX, tipY
+        for (let n = 1; n <= this.harmonics; n++) {
+            let r = this.amp / (2 * n - 1)
+            let x = r * Math.cos(
+                (2 * n - 1) * 2 * Math.PI * this.freq * this.t / 180
+            )
+            let y = -r * Math.sin(
+                (2 * n - 1) * 2 * Math.PI * this.freq * this.t / 180
+            )
+
+            ctx.strokeStyle = rgbToHex(255, 255, 255)
+            ctx.beginPath()
+            ctx.arc(x0 - xOffset, y0, r, 0, 2 * Math.PI)
+            ctx.stroke()
+
             ctx.fillStyle = rgbToHex(255, 255, 255)
             ctx.beginPath()
-            ctx.arc(x, y, 2, 0, 2 * Math.PI)
+            tipX = x0 + x - xOffset
+            tipY = y0 + y
+            ctx.arc(tipX, tipY, 3, 0, 2 * Math.PI)
             ctx.fill()
+
+            x0 = x
+            y0 = y
+        }
+
+        return {
+            x: tipX,
+            y: tipY
         }
     }
 }
@@ -46,8 +97,14 @@ function init() {
 
     fourier = new FourierSeries({
         amp: 100,
-        lambda: xTranslate / 4,
-        n: 4,
+        lambda: xTranslate,
+        n: 2,
+        freq: 1
+    })
+
+    armature = new Armature({
+        amp: 100,
+        n: 2,
         freq: 1
     })
 
@@ -60,7 +117,14 @@ function draw() {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     ctx.translate(xTranslate, yTranslate)
+
+    let armTip = armature.draw()
     fourier.draw()
+    ctx.strokeStyle = rgbToHex(128, 128, 128)
+    ctx.beginPath()
+    ctx.moveTo(armTip.x, armTip.y)
+    ctx.lineTo(0, armTip.y)
+    ctx.stroke()
 }
 
 function toHex(n) {
