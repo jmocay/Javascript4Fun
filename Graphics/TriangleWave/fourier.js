dd6ccda6f934784897ec6b5204f2399fcd9ce25f
Joel Ocay <jmocay72@gmail.com>
2020-05-30 19:18:24 -0400

    Triangle Wave

---
+++ Graphics/TriangleWave/fourier.js
@@ -0,0 +1,167 @@
+let canvas
+let ctx
+let xTranslate
+let yTranslate
+let armature
+let fourier
+
+class FourierSeries {
+    constructor(initArgs) {
+        this.amp = initArgs.amp
+        this.lambda = initArgs.lambda
+        this.freq = initArgs.freq
+        this.harmonics = initArgs.n
+        this.t = 0
+    }
+
+    draw() {
+        this.t += 1
+        if (this.t == 360) {
+            this.t = 0
+        }
+
+        let xprev, yprev
+        for (let x = 0; x < xTranslate; x++) {
+            let y = 0
+            for (let k = 0; k < this.harmonics; k++) {
+                y += -Math.sin(
+                    2 * Math.PI * (2*k + 1) * (this.freq * this.t / 180 + x / this.lambda)
+                ) * Math.pow(-1, k) * Math.pow(2*k + 1, -2)
+            }
+            y = this.amp * 8 / Math.pow(Math.PI, 2) * y
+            if (x > 1) {
+                ctx.strokeStyle = rgbToHex(0, 255, 0)
+                ctx.lineWidth = 1
+                ctx.moveTo(x, y)
+                ctx.lineTo(xprev, yprev)
+                ctx.stroke()
+            }
+
+            ctx.fillStyle = rgbToHex(0, 255, 0)
+            ctx.beginPath()
+            ctx.arc(x, y, .5, 0, 2 * Math.PI)
+            ctx.fill()
+
+            xprev = x
+            yprev = y
+        }
+    }
+}
+
+class Armature {
+    constructor(initArgs) {
+        this.amp = initArgs.amp
+        this.freq = initArgs.freq
+        this.harmonics = initArgs.n
+        this.t = 0
+    }
+
+    draw() {
+        this.t += 1
+        if (this.t == 360) {
+            this.t = 0
+        }
+
+        let xOffset = xTranslate / 2
+        let x0 = 0
+        let y0 = 0
+        let tipX, tipY
+        for (let k = 0; k < this.harmonics; k++) {
+
+            let r = this.amp * 8 * Math.pow(-1, k) * Math.pow(2*k + 1, -2) / Math.pow(Math.PI, 2)
+            let theta = 2 * Math.PI * (2*k + 1) * this.freq * this.t / 180
+            let x = r * Math.cos(theta)
+            let y = -r * Math.sin(theta)            
+
+            ctx.strokeStyle = rgbToHex(255, 255, 255)
+            ctx.lineWidth = 1
+            ctx.beginPath()
+            ctx.arc(x0 - xOffset, y0, Math.abs(r), 0, 2 * Math.PI)
+            ctx.stroke()
+
+            let ptx = x0 + x - xOffset
+            let pty = y0 + y
+
+            ctx.strokeStyle = rgbToHex(255, 255, 255)
+            ctx.lineWidth = .5
+            ctx.beginPath()
+            ctx.moveTo(x0 - xOffset, y0)
+            ctx.lineTo(ptx, pty)
+            ctx.stroke()
+
+            ctx.beginPath()
+            if (k == this.harmonics-1) {
+                ctx.fillStyle = rgbToHex(255, 255, 255)
+                ctx.arc(ptx, pty, 4, 0, 2 * Math.PI)
+                tipX = ptx
+                tipY = pty
+            }
+            else {
+                ctx.fillStyle = rgbToHex(0, 200, 255)
+                ctx.arc(ptx, pty, 3, 0, 2 * Math.PI)
+            }
+            ctx.fill()
+
+            x0 += x
+            y0 += y
+        }
+
+        return {
+            x: tipX,
+            y: tipY
+        }
+    }
+}
+
+function init() {
+    canvas = document.querySelector(".drawing__area")
+    canvas.width = 1024
+    canvas.height = 768
+    ctx = canvas.getContext("2d")
+
+    xTranslate = Math.floor(canvas.width / 2)
+    yTranslate = Math.floor(canvas.height / 2)
+
+    fourier = new FourierSeries({
+        amp: 100,
+        lambda: xTranslate / 2,
+        n: 3,
+        freq: 1
+    })
+
+    armature = new Armature({
+        amp: 100,
+        n: 3,
+        freq: 1
+    })
+
+    setInterval(draw, 64) // set this to 16 for approx 60 frames/sec refresh rate
+}
+
+function draw() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.fillStyle = rgbToHex(0, 0, 0)
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.translate(xTranslate, yTranslate)
+
+    let armTip = armature.draw()
+    // console.log(armTip)
+    fourier.draw()
+    ctx.strokeStyle = rgbToHex(128, 128, 128)
+    ctx.lineWidth = 1
+    ctx.beginPath()
+    ctx.moveTo(armTip.x, armTip.y)
+    ctx.lineTo(0, armTip.y)
+    ctx.stroke()
+}
+
+function toHex(n) {
+    return n.toString(16).padStart(2, '0').toUpperCase()
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + toHex(r) + toHex(g) + toHex(b)
+}
+
+window.addEventListener('load', init)
